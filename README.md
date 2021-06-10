563678b9aa2f32a26014490f704e5bbf9ff48abd
Justin <jreaylayton@gmail.com>
2021-06-09 22:38:30 -0700

    Updated game to include functional GUI.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rock-paper-scissors-1
+# The Odin Project - Web Development #
+## Project: Rock Paper Scissors (With GUI) ##
+In this project I revisited my previous implementation of a console based Rock Paper Scissors game
+and added a fully functional GUI to the game (yay!). I struggled a bit with changing the DOM through Javascript
+when getting an element by its ID because I was using hashtags when it wasn't necessary: document.getElementByID("#element")
+- WRONG!. Once, I figured that out I was able to finish it. There are still improvements to make in both my HTML structure
+and Javascript code.
+
+### What I've learned from this project: ###
+* Adding action event listeners to HTML objects with Javascript
+* How to manipulate the DOM and change HTML text using Javascript
+* How to manipulate the DOM and add/remove HTML elements using Javascript
+* How to add/edit inline CSS styles using Javascript
+* How to enalbe/disable inputs using Javascript
---
+++ rps.js
@@ -0,0 +1,61 @@
+const ROUNDS = 5;
+let playerScore = 0;
+let computerScore = 0;
+let currRound = 1;
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+    });
+});
+
+function computerPlay() {
+    rand = Math.floor(Math.random() * 3) + 1;
+    if (rand === 1) { return "rock"; }
+    else if (rand === 2) { return "paper"; }
+    else { return "scissors"; }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelectFormatStr = playerSelection.toLowerCase();
+    computerSelectFormatStr = computerSelection.toLowerCase();
+
+    currRound++;
+    document.getElementById("round-number").textContent = currRound;
+
+    // Tie round
+    if (playerSelectFormatStr === computerSelectFormatStr) {
+        document.getElementById("status-msg").textContent = "Tie round!";
+    }
+
+    // Player wins
+    else if ((playerSelectFormatStr === "rock" && computerSelectFormatStr === "scissors") ||
+            (playerSelectFormatStr === "paper" && computerSelectFormatStr === "rock") ||
+            (playerSelectFormatStr === "scissors" && computerSelectFormatStr === "paper")) {
+                document.getElementById("status-msg").textContent = "Player wins round!";
+                playerScore++;
+                document.getElementById("player-score").textContent = playerScore;
+    }
+
+    // Computer wins
+    else if ((computerSelectFormatStr === "rock" && playerSelectFormatStr === "scissors") ||
+            (computerSelectFormatStr === "paper" && playerSelectFormatStr === "rock") ||
+            (computerSelectFormatStr === "scissors" && playerSelectFormatStr === "paper")) {
+                document.getElementById("status-msg").textContent = "Computer wins round!";
+                computerScore++;
+                document.getElementById("computer-score").textContent = computerScore;
+    }
+
+    if (playerScore === 5 || computerScore === 5) {
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
+        if (playerScore === 5) {
+            document.getElementById("status-msg").textContent = "Player wins game. Hit F5 to play again!";
+        }
+        else {
+            document.getElementById("status-msg").textContent = "Computer wins game. Hit F5 to play again!";
+        }
+    }
+}
